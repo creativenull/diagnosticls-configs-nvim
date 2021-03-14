6a8d3ca35289fe8f72b8fbdeef45eb24b8bcb393
Arnold Chand <creativenull@outlook.com>
2021-03-13 21:21:33 -0400

    feat: linters: add rubocop

---
+++ lua/diagnosticls-nvim/linters/rubocop.lua
@@ -0,0 +1,25 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+  sourceName = 'rubocop',
+  command = fs.get_executable('bundle'),
+  debounce = 100,
+  args = [ 'exec', 'rubocop', '--format', 'json', '--force-exclusion', '--stdin', '%filepath' ],
+  parseJson = {
+    errorsRoot = 'files[0].offenses',
+    line = 'location.start_line',
+    endLine = 'location.last_line',
+    column = 'location.start_column',
+    endColumn = 'location.end_column',
+    message = '[${cop_name}] ${message}',
+    security = 'severity',
+  },
+  securities = {
+    fatal = 'error',
+    error = 'error',
+    warning = 'warning',
+    convention = 'info',
+    refactor = 'info',
+    info = 'info',
+  },
+}
