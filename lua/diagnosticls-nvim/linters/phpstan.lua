2f65ee96719e9ba758af92248019b884a912d0f4
Arnold Chand <creativenull@outlook.com>
2021-03-13 21:20:28 -0400

    feat: linters: add phpstan

---
+++ lua/diagnosticls-nvim/linters/phpstan.lua
@@ -0,0 +1,23 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+  sourceName = 'phpstan',
+  command = fs.get_executable('phpstan', 'php'),
+  args = { 'analyze', '--error-format', 'raw', '--no-progress', '%file' },
+  debounce = 100,
+  offsetLine = 0,
+  offsetColumn = 0,
+  formatLines = 1,
+  formatPattern = {
+    [[^[^:]+:(\d+):(.*)(\r|\n)*$]],
+    {
+      line = 1,
+      message = 2,
+    },
+  },
+  rootPatterns = {
+    '.git',
+    'vendor',
+    'composer.json',
+  },
+}
