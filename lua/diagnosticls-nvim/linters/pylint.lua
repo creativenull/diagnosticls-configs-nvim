e7ef7375c4ba4547e51b6a27437b2a4fbaf44e42
Arnold Chand <creativenull@outlook.com>
2021-03-13 21:20:53 -0400

    feat: linters: add pylint

---
+++ lua/diagnosticls-nvim/linters/pylint.lua
@@ -0,0 +1,39 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+  sourceName = 'pylint',
+  command = fs.get_executable('pylint'),
+  args = {
+    '--output-format',
+    'text',
+    '--score',
+    'no',
+    '--msg-template',
+    [['{line}:{column}:{category}:{msg} ({msg_id}:{symbol})']],
+    '%file',
+  },
+  offsetColumn = 1,
+  formatLines = 1,
+  formatPattern = {
+    [[^(\d+?):(\d+?):([a-z]+?):(.*)$]],
+    {
+      line = 1,
+      column = 2,
+      security = 3,
+      message = 4
+    }
+  },
+  securities = {
+    informational = 'hint',
+    refactor = 'info',
+    convention = 'info',
+    warning = 'warning',
+    error = 'error',
+    fatal = 'error'
+  },
+  rootPatterns = {
+    '.git',
+    'pyproject.toml',
+    'setup.py',
+  },
+}
