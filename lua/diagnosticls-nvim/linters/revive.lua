10ae509750808d66592f7851383c2d429e714954
Arnold Chand <creativenull@outlook.com>
2021-03-13 21:21:23 -0400

    feat: linters: add revive

---
+++ lua/diagnosticls-nvim/linters/revive.lua
@@ -0,0 +1,20 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+  sourceName = 'revive',
+  command = fs.get_executable('revive'),
+  args = { '%file' },
+  debounce = 100,
+  formatPattern = {
+    [[^[^:]+:(\d+):(\d+):\s+(.*)$]],
+    {
+      line = 1,
+      column = 2,
+      message = { 3 },
+    },
+  },
+  rootPatterns = {
+    '.git',
+    'go.mod',
+  },
+}
