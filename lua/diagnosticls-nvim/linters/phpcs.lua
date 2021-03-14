4ad0f100f42f359b6a3457dd2f2f65470bc4d2e7
Arnold Chand <creativenull@outlook.com>
2021-03-13 21:20:16 -0400

    feat: linters: add phpcs

---
+++ lua/diagnosticls-nvim/linters/phpcs.lua
@@ -0,0 +1,29 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+  sourceName = 'phpcs',
+  command = fs.get_executable('phpcs', 'php'),
+  debounce = 100,
+  args = { '--standard=PSR2', '--report=emacs', '-s', '-' },
+  offsetLine = 0,
+  offsetColumn = 0,
+  formatLines = 1,
+  formatPattern = {
+    [[^.*:(\d+):(\d+):\s+(.*)\s+-\s+(.*)(\r|\n)*$]],
+    {
+      line = 1,
+      column = 2,
+      message = 4,
+      security = 3,
+    },
+  },
+  securities = {
+    error = 'error',
+    warning = 'warning',
+  },
+  rootPatterns = {
+    '.git',
+    'vendor',
+    'composer.json',
+  },
+}
