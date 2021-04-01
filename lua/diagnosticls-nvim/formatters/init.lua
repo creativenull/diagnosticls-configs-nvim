190a44265786a86f062de3b7be6a4734d756ef38
Arnold Chand <creativenull@outlook.com>
2021-04-01 17:57:22 -0400

    fix: remove init.lua, import via modules

--- lua/diagnosticls-nvim/formatters/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  prettier = require 'diagnosticls-nvim.formatters.prettier',
-  prettier_eslint = require 'diagnosticls-nvim.formatters.prettier_eslint',
-  prettier_standard = require 'diagnosticls-nvim.formatters.prettier_standard',
-  standard_fmt = require 'diagnosticls-nvim.formatters.standard',
-  ts_standard_fmt = require 'diagnosticls-nvim.formatters.ts_standard',
-  xo_fmt = require 'diagnosticls-nvim.formatters.xo',
-}
