5eb095edd92256b552e42e46c060db1bc496e9d5
Arnold Chand <creativenull@outlook.com>
2021-04-03 22:33:05 -0400

    todo(linters): add flow

---
+++ lua/diagnosticls-nvim/linters/flow.lua
@@ -0,0 +1,27 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+-- TODO
+-- Fix this
+return {
+  sourceName = 'flow',
+  command = fs.get_executable('flow', 'node'),
+  debounce = 100,
+  args = { 'check-contents', '--json', '<', '%filepath' },
+  parseJson = {
+    errorsRoot = 'errors',
+    line = 'message[0].loc.start.line',
+    column = 'message[0].loc.start.column',
+    endLine = 'message[0].loc.end.line',
+    endColumn = 'message[0].loc.end.column',
+    message = '${message[0].descr} [flow]',
+    security = 'level',
+  },
+  securities = {
+    error = 'error',
+    warning = 'warning',
+  },
+  rootPatterns = {
+    '.flowconfig',
+    '.git'
+  },
+}
