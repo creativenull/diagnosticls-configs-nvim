bbf0db86fb27128148cd0b2485ecb1e9f202ab0d
Esteban Beltran <sergies@gmail.com>
2021-07-07 18:03:58 +0200

    Add eslint formatter

---
+++ lua/diagnosticls-nvim/formatters/eslint.lua
@@ -0,0 +1,13 @@
+local fs = require 'diagnosticls-nvim.fs'
+
+return {
+    sourceName = 'eslint',
+    command = fs.get_executable('eslint', 'node'),
+    args = {
+        '--fix', '--fix-to-stdout', '--stdin', '--stdin-filename', '%filepath'
+    },
+    rootPatterns = {
+        '.eslintrc', '.eslintrc.cjs', '.eslintrc.js', '.eslintrc.json',
+        '.eslintrc.yaml', '.eslintrc.yml'
+    }
+}
