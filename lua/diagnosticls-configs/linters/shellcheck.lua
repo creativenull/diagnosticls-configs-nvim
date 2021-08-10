22580b3af6a787257185d900997eb9873ef18741
Andy Russell <arussell123@gmail.com>
2021-08-10 10:19:39 -0400

    shellcheck: add config

---
+++ lua/diagnosticls-configs/linters/shellcheck.lua
@@ -0,0 +1,24 @@
+local fs = require 'diagnosticls-configs.fs'
+
+return {
+  sourceName = 'shellcheck',
+  command = fs.get_executable('shellcheck'),
+  debounce = 100,
+  args = { '--format', 'json1', '-' },
+  parseJson = {
+    errorsRoot = 'comments',
+    sourceName = 'file',
+    line = 'line',
+    column = 'column',
+    endLine = 'endLine',
+    endColumn = 'endColumn',
+    security = 'level',
+    message = '[shellcheck] ${message} [SC${code}]',
+  },
+  securities = {
+    error = 'error',
+    warning = 'warning',
+    info = 'info',
+    style = 'hint',
+  },
+}
