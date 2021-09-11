67989b1c66fcfe71ffa4266867f10d56680a86fa
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-11 13:03:32 -0500

    Added swiftlint default configuration.

---
+++ lua/diagnosticls-configs/linters/swiftlint.lua
@@ -0,0 +1,25 @@
+local fs = require 'diagnosticls-configs.fs'
+
+return {
+  sourceName = 'swiftlint',
+  command = fs.get_executable('swiftlint'),
+  debounce = 100,
+  args = { 
+    'lint', 
+    '%filepath', 
+    '--quiet', 
+    '--reporter', 
+    'json',
+  },
+  parseJson = {
+    line = 'line',
+    column = 'character',
+    message = '${reason} [${rule_id}]',
+    security = 'severity',
+  },
+  securities = {
+    Warning = 'warning',
+    Error = 'error',
+  },
+  rootPatterns = { 'Package.swift', '.git' },
+}
