8e6fa134ff0f1eede85dc66870e862c2eed8537f
Arnold Chand <creativenull@outlook.com>
2021-09-27 22:06:16 -0400

    feat(formatters): add stylua

---
+++ lua/diagnosticls-configs/formatters/stylua.lua
@@ -0,0 +1,7 @@
+return {
+  sourceName = 'stylua',
+  command = 'stylua',
+  args = {'--color', 'Never', '-'},
+  requiredFiles = {'stylua.toml', '.stylua.toml'},
+  rootPatterns = {'stylua.toml', '.stylua.toml'},
+}
