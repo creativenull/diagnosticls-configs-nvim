a581fa0cb597ca41af26316f652fd53f102adfb3
Arnold Chand <creativenull@outlook.com>
2021-09-27 22:05:53 -0400

    feat(project): add local config file, for internal setup

---
+++ .vim/init.lua
@@ -0,0 +1,11 @@
+-- Powered by projectlocal-vim
+-- https://github.com/creativenull/projectlocal-vim
+local ok, dls = pcall(require, 'diagnosticls-configs')
+if ok then
+  dls.setup({
+    lua = {
+      linter = require('diagnosticls-configs.linters.luacheck'),
+      formatter = require('diagnosticls-configs.formatters.lua_format'),
+    },
+  })
+end
