5f83c2af66992ea8ebb7605119667df8013f945e
Arnold Chand <creativenull@outlook.com>
2021-10-14 20:34:10 -0400

    feat(formatters/eslint_d): for formatting

---
+++ lua/diagnosticls-configs/formatters/eslint_d_fmt.lua
@@ -0,0 +1,20 @@
+local fs = require 'diagnosticls-configs.fs'
+
+return {
+  sourceName = 'eslint_d_fmt',
+  command = fs.get_executable('eslint_d', 'node'),
+  args = {
+    '--fix-to-stdout',
+    '--stdin',
+    '--stdin-filename',
+    '%filepath',
+  },
+  rootPatterns = {
+    '.eslintrc',
+    '.eslintrc.cjs',
+    '.eslintrc.js',
+    '.eslintrc.json',
+    '.eslintrc.yaml',
+    '.eslintrc.yml',
+  },
+}
