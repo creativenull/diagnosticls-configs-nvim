e58d2cf4db84ff6709f7576164e8bc71f974c3ac
Arnold Chand <creativenull@outlook.com>
2021-12-05 22:19:12 -0400

    fix(formatters/phpcbf): add args for tests

--- lua/diagnosticls-configs/formatters/phpcbf.lua
+++ lua/diagnosticls-configs/formatters/phpcbf.lua
@@ -3,6 +3,7 @@ local fs = require('diagnosticls-configs.fs')
 return {
   sourceName = 'phpcbf',
   command = fs.executable('phpcbf', fs.Scope.COMPOSER),
+  args = { '-' },
   isStdout = true,
   ignoreExitCode = true,
   rootPatterns = { 'composer.json' },
