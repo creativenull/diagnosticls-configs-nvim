27f46ccb98c59d138b60d016b2e76b4b75540d26
Gonzalo <gonzalorehu@gmail.com>
2023-01-18 01:01:28 -0500

    Created yamllint config.

---
+++ lua/diagnosticls-configs/linters/yamllint.lua
@@ -0,0 +1,20 @@
+local fs = require('diagnosticls-configs.fs')
+
+return {
+  sourceName = 'yamllint',
+  command = fs.executable('yamllint'),
+  debounce = 100,
+  args = {
+    '-f',
+    'parsable',
+    '%filepath'
+  },
+  formatPattern = {
+    [[^.*:(\d+):(\d+): \[(\w+)\] (.*)$]],
+    { line = 1, column = 2, security = 3, message = { '[yamllint]', 4 } }
+  },
+  securities = {
+    warning = 'warning',
+    error = 'error'
+  }
+}
