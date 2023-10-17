2cb68fa91d99c77264f95a3d8918ee60c8095cd6
Arnold Chand <creativenull@outlook.com>
2023-10-17 15:08:21 -0400

    fix(health): bump min version 0.7, support new health api

--- lua/diagnosticls-configs/health/init.lua
+++ lua/diagnosticls-configs/health/init.lua
@@ -1,13 +1,30 @@
-local health
+local M = {}
 
--- Keep support for nvim 0.6/0.7 but use new vim.health for nvim >= 0.8
-if vim.fn.has('nvim-0.8') == 1 then
-  health = vim.health
-else
-  health = require('health')
+---Report ok message to :checkhealth support only for nvim >= 0.7
+---@param msg string
+---@return nil
+local function report_ok(msg)
+  if vim.fn.has('nvim-0.10') == 1 then
+    vim.health.ok(msg)
+  elseif vim.fn.has('nvim-0.8') == 1 then
+    vim.health.report_ok(msg)
+  elseif vim.fn.has('nvim-0.7') == 1 then
+    require('health').report_ok(msg)
+  end
 end
 
-local M = {}
+---Report error message to :checkhealth support only for nvim >= 0.7
+---@param msg string
+---@return nil
+local function report_error(msg)
+  if vim.fn.has('nvim-0.10') == 1 then
+    vim.health.error(msg)
+  elseif vim.fn.has('nvim-0.8') == 1 then
+    vim.health.report_error(msg)
+  elseif vim.fn.has('nvim-0.7') == 1 then
+    require('health').report_error(msg)
+  end
+end
 
 local has_issues = function()
   return _G.diagnosticls_healthcheck ~= nil and not vim.tbl_isempty(_G.diagnosticls_healthcheck)
@@ -16,10 +33,10 @@ end
 M.check = function()
   if has_issues() then
     for _, issue in pairs(_G.diagnosticls_healthcheck) do
-      health.report_error(issue)
+      report_error(issue)
     end
   else
-    health.report_ok('All checks passed')
+    report_ok('All checks passed')
   end
 end
 
