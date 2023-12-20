87111e3029a253d3572aa787341403b1da2326e6
Arnold Chand <creativenull@outlook.com>
2023-12-19 22:14:01 -0400

    feat: use efmls-configs health module

--- lua/diagnosticls-configs/health/init.lua
+++ lua/diagnosticls-configs/health/init.lua
@@ -26,17 +26,25 @@ local function report_error(msg)
   end
 end
 
-local has_issues = function()
-  return _G.diagnosticls_healthcheck ~= nil and not vim.tbl_isempty(_G.diagnosticls_healthcheck)
+local function has_errors()
+  return _G._diagnosticls and not vim.tbl_isempty(_G._diagnosticls.healthcheck.errors)
 end
 
-M.check = function()
-  if has_issues() then
-    for _, issue in pairs(_G.diagnosticls_healthcheck) do
-      report_error(issue)
+local function has_ok()
+  return _G._diagnosticls and not vim.tbl_isempty(_G._diagnosticls.healthcheck.ok)
+end
+
+function M.check()
+  if has_errors() then
+    for _, err in pairs(_G._diagnosticls.healthcheck.errors) do
+      report_error(err)
     end
-  else
-    report_ok('All checks passed')
+  end
+
+  if has_ok() then
+    for _, ok in pairs(_G._diagnosticls.healthcheck.ok) do
+      report_ok(ok)
+    end
   end
 end
 
