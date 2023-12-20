69220e9dc3ce39cfeb8029783417227080f94d9b
Arnold Chand <creativenull@outlook.com>
2023-12-19 22:21:21 -0400

    fix: bring back just the tests on files

---
+++ tests/diagnosticls-configs/formatters_spec.lua
@@ -0,0 +1,54 @@
+describe('Formatter Configurations -', function()
+  local formatters = {
+    'autoflake',
+    'autoimport',
+    'autopep8',
+    'black',
+    'deno_fmt',
+    'eslint_d_fmt',
+    'eslint_fmt',
+    'gofumpt',
+    'isort',
+    'lua_format',
+    'perlimports',
+    'perltidy',
+    'php_cs_fixer',
+    'phpcbf',
+    'pint',
+    'prettier',
+    'prettier_eslint',
+    'prettier_standard',
+    'standard_fmt',
+    'stylua',
+    'swiftformat',
+    'ts_standard_fmt',
+    'xo_fmt',
+    'yapf',
+  }
+
+  local function assert_config(config)
+    local is_non_empty_string = function(value)
+      return type(value) == 'string' and value ~= ''
+    end
+
+    local is_non_empty_list = function(value)
+      return vim.tbl_islist(value) and not vim.tbl_isempty(value)
+    end
+
+    vim.validate({
+      sourceName = { config.sourceName, is_non_empty_string, 'non-empty string' },
+      command = { config.command, is_non_empty_string, 'non-empty string' },
+      args = { config.args, is_non_empty_list, 'non-empty list/table' },
+    })
+  end
+
+  -- Start tests
+  for _, formatter in pairs(formatters) do
+    it(string.format('Assert %q formatter', formatter), function()
+      local fmt_config = require('diagnosticls-configs.formatters.' .. formatter)
+      assert.has_no.errors(function()
+        assert_config(fmt_config)
+      end)
+    end)
+  end
+end)
---
+++ tests/diagnosticls-configs/linters_spec.lua
@@ -0,0 +1,60 @@
+describe('Linter Configurations -', function()
+  local linters = {
+    'alex',
+    'cpplint',
+    'eslint',
+    'eslint_d',
+    'flake8',
+    'flow',
+    'golangci_lint',
+    'languagetool',
+    'luacheck',
+    'mypy',
+    'perlcritic',
+    'phpcs',
+    'phpstan',
+    'proselint',
+    'psalm',
+    'pylint',
+    'redpen',
+    'reek',
+    'revive',
+    'rubocop',
+    'shellcheck',
+    'standard',
+    'stylelint',
+    'swiftformat',
+    'swiftlint',
+    'ts_standard',
+    'vint',
+    'vulture',
+    'xo',
+    'yamllint',
+  }
+
+  local function assert_config(config)
+    local is_non_empty_string = function(value)
+      return type(value) == 'string' and value ~= ''
+    end
+
+    local is_non_empty_list = function(value)
+      return vim.tbl_islist(value) and not vim.tbl_isempty(value)
+    end
+
+    vim.validate({
+      sourceName = { config.sourceName, is_non_empty_string, 'non-empty string' },
+      command = { config.command, is_non_empty_string, 'non-empty string' },
+      args = { config.args, is_non_empty_list, 'non-empty list/table' },
+    })
+  end
+
+  -- Start tests
+  for _, linter in pairs(linters) do
+    it(string.format('Assert %q linter', linter), function()
+      local lint_config = require('diagnosticls-configs.linters.' .. linter)
+      assert.has_no.errors(function()
+        assert_config(lint_config)
+      end)
+    end)
+  end
+end)
